3bdabf3b4b802a1e6b5f925f7cea057c7597a616
luccanog <luccathomaz@gmail.com>
2023-10-29 14:20:54 -0300

    using appsettings configuration on consumer worker

--- Kafka.Dotnet.Consumer/Worker.cs
+++ Kafka.Dotnet.Consumer/Worker.cs
@@ -1,36 +1,42 @@
-using System.Collections.Generic;
 using Confluent.Kafka;
-using static Confluent.Kafka.ConfigPropertyNames;
 
 namespace Kafka.Dotnet.Consumer
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
-        private readonly IConsumer<Ignore,string> _consumer;
+        private readonly IConsumer<Ignore, string> _consumer;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
             _logger = logger;
 
             var config = new ConsumerConfig
             {
-                BootstrapServers = "host1:9092,host2:9092",
-                GroupId = "foo",
+                BootstrapServers = configuration["Kafka:BootstrapServers"],
+                GroupId = configuration["Kafka:GroupId"],
                 AutoOffsetReset = AutoOffsetReset.Earliest
             };
 
             _consumer = new ConsumerBuilder<Ignore, string>(config).Build();
-           
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            _consumer.Subscribe("topic");
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+
+                var consumeResult = _consumer.Consume(stoppingToken);
             }
+            _consumer.Close();
+        }
+
+        ~Worker()
+        {
+            _consumer.Dispose();
         }
     }
 }
