08c5d8c7c0b3ab059ec2e714558f8958c822c494
luccanog <luccathomaz@gmail.com>
2023-10-30 19:16:43 -0300

    removing storage folder from Dotnet.API project

--- Kafka.Dotnet.API/Storage/IReadonlyStorage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Kafka.Dotnet.API.Storage
-{
-    public interface IReadonlyStorage<T> where T : class
-    {
-        public IEnumerable<T> GetAll();
-
-        public T? GetValue(Guid id);
-    }
-}
